b5d6fe1918a224670b58119bb5ec3486400ccb30
Zakarias Januaji <zjanuaji@gmail.com>
2021-10-18 15:31:20 +0700

    Update route.py

--- vision/route.py
+++ vision/route.py
@@ -5,8 +5,8 @@ Background image is the 1872 world map.
 Front image is your profile picture.
 
 Steps to do:
-1. Generate a route.
-2. Run and save.
+1. You generate a route.
+2. This script runs and saves the result.
 
 Expected commands:
 $ python route.py to/back.jpg to/front.png to/route.json to/route.mp4
@@ -15,6 +15,10 @@ $ python route.py gen back.png route.json
 will generate route.json
 $ python route.py run back.jpg front.jpg route.json route.mp4
 will run and save route.mp4 frame by frame
+'''
+
+UNABLE = '''
+This script can't do it beautifully.
 '''
 
 import cv2
